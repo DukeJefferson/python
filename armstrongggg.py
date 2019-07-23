a21bf904b274ff668b40393f115623c98d3d0e76
DukeJefferson <53052891+DukeJefferson@users.noreply.github.com>
2019-07-23 15:42:10 +0530

    Update armstrongggg.py

--- armstrongggg.py
+++ armstrongggg.py
@@ -2,12 +2,12 @@ n=int(input())
 m=int(input())
 for a in range(n,m+1):
   temp=a
-  sum = 0
-  while temp > 0:
+  sum=0
+  while (temp>0):
     digit=temp%10
     sum+=digit**3
-    temp //=10
-    if sum==a:
+    temp//=10
+    if (sum==a):
       b[]=a
 print(*b)
     
